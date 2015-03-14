2f1f3942f7453922da31e561038a47119125e159
hkuro <kuro2357509@gmail.com>
2015-03-14 22:29:41 +0800

    Try to evaluate the efficiency of the Fibonacci and compare recursive and non-recursive performance.
    Modify fibseq.c with adding time.h clock() to compute time of fibonacci.
    Modify the parameters larger by modifing Makefile.

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 extern int fibonacci(int x);
 
@@ -6,11 +7,20 @@ int main(int argc, char **argv)
 {
   int number=0;
   int result=0;
-
+  time_t start_tm, finish_tm;
+  
   scanf("%d",&number);
-  result = fibonacci(number);   
+ 
+  start_tm = clock();
+  result = fibonacci(number);
+  finish_tm = clock();
+
   printf("The fibonacci sequence at %d is: %d\n", number, result);
   
+  float elapsed_tm = (float)(finish_tm - start_tm)/ CLOCKS_PER_SEC;
+
+  printf("Time: %f seconds\n", elapsed_tm);
+
   return(0);
 }
 
